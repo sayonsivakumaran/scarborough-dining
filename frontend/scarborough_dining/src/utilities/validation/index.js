4bc2feaa9822804bb60f13deeb8b73528ae56cd3
Sayon Sivakumaran <sayon.sivakumaran@gmail.com>
2020-07-03 20:59:51 -0400

    Add validation documentation

--- frontend/scarborough_dining/src/utilities/validation/index.js
+++ frontend/scarborough_dining/src/utilities/validation/index.js
@@ -1,3 +1,7 @@
+/**
+ * Determines if the inputted file is an image file.
+ * @param file The file that will be checked
+ */
 export const isImageFile = file => {
     return file && file.type.split('/')[0] === 'image';
 }

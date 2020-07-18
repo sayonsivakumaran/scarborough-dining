7656006d40abcb5d9b3ab7113e910accb6afc082
Justin <justin.tran.jjt@outlook.com>
2020-07-17 20:59:03 -0400

    moved dotenv to line 16 in server.js

--- backend/scarborough_dining/server.js
+++ backend/scarborough_dining/server.js
@@ -12,7 +12,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(fileupload({ useTempFiles: true }));
 app.use(express.json());
-require('dotenv').config();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("../../frontend/scarborough_dining/build"));
+} else {
+    require('dotenv').config();
+}
 
 /**
  * Connect server to MongoDB using environment variables.
@@ -38,12 +43,6 @@ app.use('/owners', ownersRouter);
 app.use('/restaurants', restaurantsRouter);
 app.use('/menu_items', menuItemsRouter);
 app.use('/media_upload', uploadMediaItemsRouter);
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("../../frontend/scarborough_dining/build"));
-} else {
-    require('dotenv').config();
-}
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);

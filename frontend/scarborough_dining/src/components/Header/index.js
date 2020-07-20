b64553256fcbadf74f93ce141b75cee0016078fc
riyasat <riyasattalukder@gmail.com>
2020-07-19 23:48:26 -0400

    Added the code to render the search component in the header

--- frontend/scarborough_dining/src/components/Header/index.js
+++ frontend/scarborough_dining/src/components/Header/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import GoogleBtn from '../Header/GoogleBtn';
 import {Link} from 'react-router-dom';
 import './styles.css';
+import { SearchField } from '../Search/searchField';
 
 export class Header extends Component {
 
@@ -43,6 +44,7 @@ export class Header extends Component {
                     <Link class="link" to="/account-creation/restaurant">Register Restaurant</Link>
                     <Link class="link" to="/manage-restaurant-information">Manage Restaurant Information</Link>
                     <Link class="link"><GoogleBtn login={this.setLoggedIn.bind(this)} logout={this.setLoggedOut.bind(this)} className="google-btn"/></Link>
+                    <SearchField></SearchField>
                 </div>
             </div>
             </header>

0db63a470d4b842c1b7be27226e6a0f4a0c848b2
riyasat <riyasattalukder@gmail.com>
2020-07-26 00:19:35 -0400

    Added a handle submit method

--- frontend/scarborough_dining/src/components/Search/searchField.js
+++ frontend/scarborough_dining/src/components/Search/searchField.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
 import './styles.css';
+import axios from 'axios';
 
 export class SearchField extends Component{
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            searchString: ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    async handleSubmit(event)
+    {
+        event.preventDefault();
+        let results = await axios.get('/restaurants/search_results', {
+            params: {
+              queryString: this.state.searchString
+            }
+          });
+        console.log(results);
+    }
 
     render() {
         return(
         <div className="search-container">
-            <form>
+            <form onSubmit={this.handleSubmit}> 
                 <input 
                     name="searchString"
                     type="text"

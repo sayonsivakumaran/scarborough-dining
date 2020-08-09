68e2d9047bed14ca5bfdb3a47b4cef1e2caae403
Sayon Sivakumaran <sayon.sivakumaran@gmail.com>
2020-08-08 20:18:04 -0400

    Seperate tables in shopping cart by restaurant

--- frontend/scarborough_dining/src/components/ShoppingCart/index.js
+++ frontend/scarborough_dining/src/components/ShoppingCart/index.js
@@ -38,12 +38,11 @@ class ShoppingCart extends Component {
             .catch(err => err);
     }
 
-    _getShoppingCartItems = shoppingCart => {
-        let shoppingCartTables = [];
-        
-        for (var i = 0; i < shoppingCart.length; i++) {
+    _getShoppingCartItemsForRestaurant = (shoppingCart) => {
+        let shoppingCartItemTables = [];
+        for (let i = 0, l = shoppingCart.length; i < l; i++) {
             let id = shoppingCart[i].menuItemID;
-            shoppingCartTables.push(
+            shoppingCartItemTables.push(
                 <tr className="unverified-restaurant-row" key={id}>
                     <td><img className="shopping-cart-image" src={shoppingCart[i].imageURL}/></td>
                     <td>{shoppingCart[i].name}</td>
@@ -56,7 +55,44 @@ class ShoppingCart extends Component {
                 </tr>
             )
         }
-        return shoppingCartTables;
+            
+        return (
+            <table className="table table-responsive table-hover">
+                <thead class="table-header">
+                    <tr className="t-header">
+                        <th scope="col">Item Image</th>
+                        <th scope="col">Item Name</th>
+                        <th scope="col">Item Price</th>
+                        <th scope="col">Total Items</th>
+                        <th scope="col">Total Cost</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {shoppingCartItemTables}
+                </tbody>
+            </table>
+        );
+    }
+
+    _getShoppingCartItems = shoppingCart => {
+        let restaurantOrderMap = {};
+
+        for (let i = 0; i < shoppingCart.length; i++) {
+            let restaurantID = shoppingCart[i].restaurantID;
+            if (!restaurantOrderMap[restaurantID]) {
+                restaurantOrderMap[restaurantID] = [];
+            }
+
+            restaurantOrderMap[restaurantID] = restaurantOrderMap[restaurantID].concat([shoppingCart[i]]);
+        }
+        
+        let shoppingCartTableArray = [];
+        Object.keys(restaurantOrderMap).forEach(restaurantID => {
+            shoppingCartTableArray.push(this._getShoppingCartItemsForRestaurant(restaurantOrderMap[restaurantID]));
+        });
+
+        return shoppingCartTableArray;
     }
 
     open = _ => {
@@ -110,24 +146,10 @@ class ShoppingCart extends Component {
                 <h2>Shopping Cart</h2>
 
                 {/* TODO: image, item name, total ordered, individual price, total price */}
-                <h4 className="total">{this.state.totalItems} Items</h4>
+                <h3 className="total">{this.state.totalItems} Total Items</h3>
                 {this.state.totalItems > 0 ? (
                     <React.Fragment>
-                        <table className="table table-responsive table-hover">
-                            <thead class="table-header">
-                                <tr className="t-header">
-                                    <th scope="col">Item Image</th>
-                                    <th scope="col">Item Name</th>
-                                    <th scope="col">Item Price</th>
-                                    <th scope="col">Total Items</th>
-                                    <th scope="col">Total Cost</th>
-                                    <th scope="col"></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {this._getShoppingCartItems(Object.values(this.state.shoppingCart))}
-                            </tbody>
-                        </table>
+                        {this._getShoppingCartItems(Object.values(this.state.shoppingCart))}
                         <input onClick={() => this._postShoppingCartData(Object.values(this.state.shoppingCart))} className="submit-shopping-cart-modal-button" name="submitShoppingCart" type="submit" value="Submit Order"/>
                     </React.Fragment>
                 ) : (

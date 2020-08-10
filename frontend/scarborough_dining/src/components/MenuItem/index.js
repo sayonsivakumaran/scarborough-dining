d50afeb134e863937282e6fc52faa31c6a9afa1d
Sayon Sivakumaran <sayon.sivakumaran@gmail.com>
2020-08-10 12:00:57 -0400

    Fix restaurant card styles

--- frontend/scarborough_dining/src/components/MenuItem/index.js
+++ frontend/scarborough_dining/src/components/MenuItem/index.js
@@ -51,8 +51,8 @@ export default class MenuItem extends Component {
     
     render() {
         return (
-            <Link className="col-md-4 d-flex align-items-stretch text-link" onClick={this.open}>
-                <div className="card menu">
+            <Link className="col-md-4 d-flex align-items-stretch text-link menu" onClick={this.open}>
+                <div className="card">
                     <img className="card-img-top" src={this.menuItem.imageURL}/>
                     <div className="card-body"> 
                         <p className="title"><b>{this.menuItem.name}</b></p>
